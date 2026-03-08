f58117670d68942c3cbd1671f5ec7fdb2bf20cd4
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-03-08 16:20:03 +0000

    fix: url for goal decomposition

--- backend/goals/urls.py
+++ backend/goals/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import DecomposeGoalView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('decompose/', DecomposeGoalView.as_view(), name='decompose-goal')
 ]
