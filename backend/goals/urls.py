311ac3dd467e5af15a59cd65e64708c2d7f35cea
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-03-16 13:07:12 +0000

    feat: add list endpoint to urlpatterns

--- backend/goals/urls.py
+++ backend/goals/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DecomposeGoalView
+from .views import DecomposeGoalView, GoalListView
 
 urlpatterns = [
-    path('decompose/', DecomposeGoalView.as_view(), name='decompose-goal')
+    path('decompose/', DecomposeGoalView.as_view(), name='decompose-goal'),
+    path('list/', GoalListView.as_view(), name="goal-list")
 ]
