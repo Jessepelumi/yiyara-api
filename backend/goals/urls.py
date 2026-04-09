1a8402b408663e8d9b972f4cb6fbe8262a3bef78
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-09 03:42:59 +0100

    feat: add URL to delete a goal

--- backend/goals/urls.py
+++ backend/goals/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DecomposeGoalView, GoalListView
+from .views import DecomposeGoalView, GoalListView, DeleteGoalView
 
 urlpatterns = [
     path('decompose/', DecomposeGoalView.as_view(), name='decompose-goal'),
-    path('list/', GoalListView.as_view(), name="goal-list")
+    path('list/', GoalListView.as_view(), name="goal-list"),
+    path('<uuid:pk>/', DeleteGoalView.as_view(), name='goal-delete'),
 ]
