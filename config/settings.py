5452656276fa23463156cd12386d50766f10cbdb
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-07-02 12:15:09 +0100

    fix: too important

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e30=ws8dd^7u4ntw))xx)!#bndn@fsp0h+8i$1ucx7=4xdl-08'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
